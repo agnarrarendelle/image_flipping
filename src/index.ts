5b306579348b55c8ea26dc2fa1765b3f301f377e
agnarrarendelle <danny20001102@gmail.com>
2022-09-19 23:08:05 +0800

    implemented filp and write image

--- src/index.ts
+++ src/index.ts
@@ -12,24 +12,25 @@ const isImageValid = (imageUrl: string) => {
 };
 
 (async () => {
-  //   const image = await Jimp.read(`${__dirname}/${imgUrl}`);
-  //   try {
-  //     const flippedImage = await image.flip(true, true);
-  //     await flippedImage.write("flip1.png");
-  //   } catch (e) {
-  //     process.exit(1);
-  //   }
-  //   console.log("done");
   if (!fs.existsSync(outputDir)) fs.mkdirSync(outputDir);
   while (true) {
     const imgUrl = prompt("Please Enter Image Url:");
-    const imgName = prompt("Please Enter Name of Image:");
     const imageValid = await isImageValid(imgUrl);
     if (!imageValid) {
       console.log("please enter a valid image url");
       continue;
     }
 
-    const image = await Jimp.read(imgUrl);
+    const imgName = prompt("Please Enter Name of Image:");
     
+
+    const image = await Jimp.read(imgUrl);
+    try {
+      const flippedImage = await image.flip(true, true);
+      await flippedImage.writeAsync(`${outputDir}/${imgName}.png`);
+    } catch (e) {
+      console.log("Cannot process your image. Please try again");
+      continue;
+    }
+  }
 })();
